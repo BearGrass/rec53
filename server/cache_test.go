0d7c76a3125f6dcee324f18554d8c54c9a88a424
BearGrass <superlong100@gmail.com>
2023-03-26 05:47:58 +0000

    del nouse code

--- server/cache_test.go
+++ server/cache_test.go
@@ -3,22 +3,7 @@ package server
 //test cache.go
 import (
 	"testing"
-
-	"github.com/miekg/dns"
 )
 
 func TestCache(t *testing.T) {
-	cache := NewCache()
-	cache.SetCacheSize(100)
-	cache.SetCache("www.baidu.com", &dns.Msg{})
-	if cache.GetCacheSize() != 100 {
-		t.Error("cache size error")
-	}
-	if _, ok := cache.GetCache()["www.baidu.com"]; !ok {
-		t.Error("cache set error")
-	}
-	cache.DeleteCache("www.baidu.com")
-	if _, ok := cache.GetCache()["www.baidu.com"]; ok {
-		t.Error("cache delete error")
-	}
 }
